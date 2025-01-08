2c3508fc3983006b190a5703ea9a35c5967d2185
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-01-08 16:58:08 -0500

    add index

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 import express from 'express'
-import userRouter from './routes/v1/user.routes.js'
-import equiposRouter from './routes/v1/equipos.routes.js'
-import jugadorRouter from './routes/v1/jugador.routes.js'
-import matcherRouter from './routes/v1/matcher.routes.js'
-import ofertasRouter from './routes/v1/ofertas.routes.js'
-import torneosRouter from './routes/v1/torneos.routes.js'
+import Routes from './routes/index.routes.js'
 import cors from 'cors'
 
 const app = express()
 
-app.use(userRouter,equiposRouter,jugadorRouter,matcherRouter,ofertasRouter,torneosRouter)
+Routes(app)
 app.use(cors())
 app.use(express.json())
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,9 @@
-import users from "./v1/user.routes"
-import torneos from "./v1/torneos.routes"
-import equipos from "./v1/equipos.routes"
-import jugadores from "./v1/jugador.routes"
-import ofertas from "./v1/ofertas.routes"
-import matcher from "./v1/matcher.routes"
+import users from "./v1/user.routes.js"
+import torneos from "./v1/torneos.routes.js"
+import equipos from "./v1/equipos.routes.js"
+import jugadores from "./v1/jugador.routes.js"
+import ofertas from "./v1/ofertas.routes.js"
+import matcher from "./v1/matcher.routes.js"
 
 export  default (app) => {
   app.use("/api/v1/users", users);
