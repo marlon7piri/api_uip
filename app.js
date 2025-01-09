6e2ce6c84f4394d9d9ac047741559983d283770b
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-01-09 08:50:49 -0500

    add bodyparser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import Routes from './routes/index.routes.js'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express()
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true,limit:'3mb' }));
+app.use(bodyParser.json({limit:'3mb'}));
 app.use(cors())
 
 Routes(app)
