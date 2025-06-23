e578a7a000affac314f4b839e2ccc2b3848e48ca
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-06-22 22:03:08 -0500

    add new config 2

--- index.ts
+++ index.ts
@@ -1,17 +1,9 @@
-
-import app from './app.js'
-import { PORT } from './config.js'
-import { ConnectDb } from './database'
-
+import app from "./app.js";
+import { PORT } from "./config.js";
+import { ConnectDb } from "./database";
 
 const main = async () => {
+  await ConnectDb();
+};
 
-    await ConnectDb()
-    app.listen(PORT, () => {
-        console.log('Server is running in port:', PORT)
-    })
-   
-   
-}
-
-main()
+main();
