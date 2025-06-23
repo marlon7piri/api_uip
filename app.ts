29ed68c58bd85d335d13850623d3d895981e3535
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-06-22 23:22:56 -0500

    add new config  11

--- app.ts
+++ app.ts
@@ -7,21 +7,19 @@ import { rateLimit } from "express-rate-limit";
 import { CORS_ORIGIN } from "config";
 
 const app = express();
+// Confía en el proxy de Vercel
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 app.use(bodyParser.json({ limit: "100mb" }));
 app.use(helmet());
 
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000,
-  message: "Demasiadas peticiones, no lo intentes mas!",
-  limit: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-  handler(req, res) {
-    return res.status(209).json({ error: "Bloqueado por rate limit" });
-  },
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // Limita cada IP a 100 peticiones por ventana
 });
+
 app.use(limiter);
 app.use(
   cors({
