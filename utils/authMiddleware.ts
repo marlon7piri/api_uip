d1abe3ffbe3afdc36aa179bd3a7f977800dcd7e7
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-06-23 00:09:04 -0500

    add new config  19

--- utils/authMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  console.log(req.headers); // nota: es `headers`, no `header`
-
-  // Aquí podrías hacer validaciones con tokens, sesiones, etc.
-
-  next();
-};
--- utils/crearJugadorInicial.ts
+++ utils/crearJugadorInicial.ts
@@ -2,38 +2,30 @@ import Jugador from "models/Jugador.models";
 import { Types } from "mongoose";
 
 export const crearJugadorInicial = async (data: any) => {
+  try {
+    const jugador = new Jugador({
+      nombre: data.nombre,
+      apellido: data.apellido,
+      edad: 18,
+      estatura: 170,
+      foto: process.env.URL_FOTO_AGENTE_LIBRE,
+      estadisticasGlobales: {
+        posicion: "Centro Campista",
+      },
 
-    try {
+      rol: data.clasificacion,
+      email: data.email,
+      estudiante: "no",
+      userId: data?.userId?.toString(),
+      club: new Types.ObjectId(process.env.ID_EQUIPO_AGENTE_LIBRE),
+    });
 
-        console.log(process.env.URL_FOTO_AGENTE_LIBRE)
-        console.log(process.env.ID_EQUIPO_AGENTE_LIBRE)
-        const jugador = new Jugador({
-            nombre: data.nombre,
-            apellido: data.apellido,
-            edad: 18,
-            estatura: 170,
-            foto: process.env.URL_FOTO_AGENTE_LIBRE,
-            estadisticasGlobales: {
-                posicion: "Centro Campista",
-            },
+    const jugadoSaved = await jugador.save();
 
-            rol: data.clasificacion,
-            email: data.email,
-            estudiante: "no",
-            userId: data?.userId?.toString(),
-            club: new Types.ObjectId(process.env.ID_EQUIPO_AGENTE_LIBRE)
-        });
-
-
-
-        console.log({ jugador })
-        const jugadoSaved = await jugador.save();
-
-        if (jugadoSaved) {
-            return true
-        }
-
-    } catch (error) {
-        throw new Error("Error creando el jugador")
+    if (jugadoSaved) {
+      return true;
     }
-}
+  } catch (error) {
+    throw new Error("Error creando el jugador");
+  }
+};
