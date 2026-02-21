3fe6dbbadfbcbc59f3a45e11264cae7c7e5fad10
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2026-02-21 00:36:09 -0500

    Update torneo.dto.ts

--- dtos/torneo.dto.ts
+++ dtos/torneo.dto.ts
@@ -1,5 +1,5 @@
 import { GrupoDTO } from "./grupo.dto";
-import { ITorneo } from "../models/torneo.model";
+import { ITorneo } from "../models/Torneo.models";
 
 export interface TorneoResponseDTO
   extends Omit<ITorneo, "grupos" | "equipos"> {
